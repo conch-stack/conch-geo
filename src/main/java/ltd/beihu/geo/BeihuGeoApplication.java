942f6a1f53491d82eb92208b6f71c98f5f1fb932
zjz6b6 <407091038@qq.com>
2019-10-16 20:59:20 +0800

    init project

---
+++ src/main/java/ltd/beihu/geo/BeihuGeoApplication.java
@@ -0,0 +1,13 @@
+package ltd.beihu.geo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeihuGeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeihuGeoApplication.class, args);
+	}
+
+}
